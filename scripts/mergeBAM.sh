d7f34cff59eab62b7c7ea2930bb1668a9dc137d2
Paul Hoffman <hoff0792@umn.edu>
2017-03-06 11:49:23 -0600

    Change script to merge using Picard MergeSamFiles instead of BAMtools merge

--- scripts/mergeBAM.sh
+++ scripts/mergeBAM.sh
@@ -3,20 +3,23 @@
 set -eo pipefail
 
 #   Check for dependencies
-$(command -v bamtools > /dev/null 2> /dev/null) || (echo "Please install BAMtools for this script" >&2; exit 1)
+# $(command -v bamtools > /dev/null 2> /dev/null) || (echo "Please install BAMtools for this script" >&2; exit 1)
+$(command -v java > /dev/null 2> /dev/null) || (echo "Please install Java for this script" >&2; exit 1)
 $(command -v parallel > /dev/null 2> /dev/null) || (echo "Please install GNU Parallel for this script" >&2; exit 1)
 
 #   Some useful global variables
 OUT_DEFAULT="$(pwd -P)/merged"
+PICARD_DEFAULT="/usr/local/bin/picard.jar"
 declare -x DELIMITER=','
 
 #   Usage message
 function Usage() {
     echo -e "\
-Usage: $0 -t|--table <table> -s|--sample-list <sample list> [-o|--outdirectory <outdirectory> \n\
+Usage: $0 -t|--table <table> -s|--sample-list <sample list> [-o|--outdirectory <outdirectory>] [-p|--picard <picard jar>]\n\
 Where:      <table> is the sample name table (see below) \n\
             <sample list> is a list of BAM files, named with the sample names in <table> \n\
             [<outdirectory>] is an optional output directory, defaults to ${OUT_DEFAULT} \n\
+            [<picard jar>] is an optional path to the Picard JAR, defaults to ${PICARD_DEFAULT} \n\
 \n\
 The sample name table is a whitespace-delimited table where the new sample name \n\
     is in the first column and the old sample names are in subsequent columns \n\
@@ -48,8 +51,12 @@ do
             SAMPLE_LIST="$2"
             shift
             ;;
-        -o|--outdirectory )
+        -o|--outdirectory)
             OUTDIR="$2"
+            shift
+            ;;
+        -p|--picard)
+            PICARD_JAR="$2"
             shift
             ;;
         *)
@@ -61,6 +68,8 @@ done
 
 #   Argument checking
 [[ -z "${OUTDIR}" ]] && OUTDIR="${OUT_DEFAULT}"
+[[ -z "${PICARD_JAR}" ]] && PICARD_JAR="${PICARD_DEFAULT}"
+[[ -f "${PICARD_JAR}" ]] || (echo "Cannot find Picard JAR file at ${PICARD_JAR}, please provide path to Picard JAR file" >&2; exit 1)
 [[ -f "${SAMPLE_LIST}" ]] || (echo "Cannot find ${SAMPLE_LIST}, exiting..." >&2; exit 1)
 [[ -f "${TABLE}" ]] || (echo "Cannot find ${TABLE}, exiting..." >&2; exit 1)
 
@@ -81,15 +90,19 @@ function merge() {
     local samplelist="$1" # Where is our sample list?
     local mergedname="$2" # What is the name of our merged sample?
     local outdir="$3" # Where do we put our output file?
-    local -a samples=($(echo $4 | tr "${DELIMITER}" ' ')) # Make an array of old sample names
+    local picardjar="$4" # Where is our Picard JAR file?
+    local -a samples=($(echo $5 | tr "${DELIMITER}" ' ')) # Make an array of old sample names
     #   Pick out our BAM files
     local -a bamfiles=($(grep --color=never -f <(echo "${samples[@]}" | tr ' ' '\n') "${samplelist}"))
+    #   Create an output name
+    local outname=""${outdir}/${mergedname}_merged.bam
     #   Merge the BAM files
-    (set -x; bamtools merge -list <(echo "${bamfiles[@]}" | tr ' ' '\n') > "${outdir}/${mergedname}_merged.bam")
-    echo "Merged bam file for sample ${mergedname} can be found at ${outdir}/${mergedname}_merged.bam" >&2
+    # (set -x; bamtools merge -list <(echo "${bamfiles[@]}" | tr ' ' '\n') > "${outdir}/${mergedname}_merged.bam")
+    (set -x; java -jar "${picardjar}" MergeSamFiles $(for s in ${bamfiles[@]}; do echo "I=$s "; done) O="${outname}")
+    echo "Merged bam file for sample ${mergedname} can be found at ${outname}" >&2
 }
 
 #   Export the function
 export -f merge
 
-parallel --verbose --xapply "merge ${SAMPLE_LIST} {1} ${OUTDIR} {2}" ::: "${!SAMPLE_NAMES[@]}" ::: "${SAMPLE_NAMES[@]}"
+parallel --verbose --xapply "merge ${SAMPLE_LIST} {1} ${OUTDIR} ${PICARD_JAR} {2}" ::: "${!SAMPLE_NAMES[@]}" ::: "${SAMPLE_NAMES[@]}"
