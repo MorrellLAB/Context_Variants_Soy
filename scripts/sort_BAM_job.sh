22d1bf8d28e817639c866014411856e8ac969243
Fernanda Rodriguez <fernandarodriguez@x-134-84-2-235.vpn.umn.edu>
2017-03-10 16:19:26 -0600

    sort merged bam files

---
+++ scripts/sort_BAM_job.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#PBS -l mem=6gb,nodes=1:ppn=16,walltime=6:00:00
+#PBS -m abe
+#PBS -M mfrodrig@umn.edu
+#PBS -q lab
+
+module load samtools
+
+samtools sort -O bam -o M92-220.x1.04.WT_sorted.bam /panfs/roc/scratch/fernanda/FN30/SAM_Processing/SAMtools/Sorted_BAM/reheader/merged/M92-220.x1.04.WT.bam
+
